e30f6e8ec0d86e493d498ddd505027c475f8d38f
Lateef Ibrahim <ibrahimlateefope@gmail.com>
2026-09-10 17:59:49 +0100

    refactor:clean up calculator logic

--- script.js
+++ script.js
@@ -1,63 +1,88 @@
-function add(a, b) {
-  return a + b;
+/*---------------------
+-------- TEXT -------
+----------------------*/
+const calculatorInput = document.querySelector(".calculation-input");
+const calculationDisplay = document.querySelector(".calculation");
+/*---------------------
+-------- KEYS -------
+----------------------*/
+const numberKeys = document.querySelectorAll(".key-num");
+const operatorKeys = document.querySelectorAll(".key-op");
+const clearKey = document.querySelector(".key-clear");
+const backKey = document.querySelector(".key-back");
+const dotKey = document.querySelector(".key-dot");
+const equalKey = document.querySelector(".key-eq");
+/*---------------------
+-------- STATES -------
+----------------------*/
+let firstInput = null;
+let secondInput = null;
+let operatorType = null;
+let resultDisplayed = false;
+/*---------------------
+-------- HELPER FUNCTIONS -------
+----------------------*/
+function clearEl(element) {
+  element.textContent = "";
 }
-
-function divide(a, b) {
-  return a / b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-function subtract(a, b) {
-  return a - b;
-}
-let input = "";
-let firstInput;
-let secondInput;
-let operationType;
-const calculationInput = document.querySelector(".calculation-input");
-const equalEl = document.querySelector(".key-eq");
-const clearEl = document.querySelector(".key-clear");
-Array.from(document.querySelectorAll(".key-num")).forEach((keyNum) => {
-  calculationInput.textContent = "";
-  keyNum.addEventListener("click", () => {
-    input = keyNum.dataset.btn;
-    calculationInput.textContent += input;
-  });
-});
-
-Array.from(document.querySelectorAll(".key-op")).forEach((operation) => {
-  operation.addEventListener("click", () => {
-    firstInput = parseInt(calculationInput.textContent);
-    calculationInput.textContent = "";
-    const dataBtn = operation.dataset.btn;
-    operationType = dataBtn;
-    console.log(operationType);
-  });
-});
-
-equalEl.addEventListener("click", () => {
-  secondInput = parseInt(calculationInput.textContent);
-  let result;
-  if (operationType === "div") {
-    result = divide(firstInput, secondInput).toFixed(1);
-  } else if (operationType === "add") {
-    result = add(firstInput, secondInput).toFixed(1);
-  } else if (operationType === "multiply") {
-    result = multiply(firstInput, secondInput).toFixed(1);
-  } else if (operationType === "sub") {
-    result = subtract(firstInput, secondInput).toFixed(1);
+function getResult(a, b) {
+  if (operatorType === "div") {
+    return a / b;
+  } else if (operatorType === "multiply") {
+    return a * b;
+  } else if (operatorType === "sub") {
+    return a - b;
+  } else if (operatorType === "add") {
+    return a + b;
   }
-clearEl.click()
-  calculationInput.textContent = result;
-  
+}
+clearEl(calculatorInput);
+numberKeys.forEach((numberKey) => {
+  numberKey.addEventListener("click", () => {
+    let numberInput = numberKey.dataset.btn;
+    if (resultDisplayed) {
+      calculatorInput.textContent = numberInput;
+      resultDisplayed = false;
+    } else {
+      calculatorInput.textContent += numberInput;
+    }
+  });
 });
 
-clearEl.addEventListener("click", () => {
-  input = "";
-  firstInput = 0;
-  secondInput = 0;
-  operationType = "";
-  calculationInput.textContent = "";
+operatorKeys.forEach((operatorKey) => {
+  operatorKey.addEventListener("click", () => {
+    if (firstInput !== null && operatorType !== null) {
+      secondInput = Number(calculatorInput.textContent);
+      let result = Number(getResult(firstInput, secondInput));
+      firstInput = result;
+      secondInput = null;
+      calculatorInput.textContent = firstInput;
+      resultDisplayed = true;
+    } else {
+      firstInput = Number(calculatorInput.textContent);
+      clearEl(calculatorInput);
+    }
+    operatorType = operatorKey.dataset.btn;
+  });
+});
+equalKey.addEventListener("click", () => {
+  if (calculatorInput.textContent === "" || operatorType === null) return;
+  secondInput = Number(calculatorInput.textContent);
+  let result = Number(getResult(firstInput, secondInput));
+  firstInput = result;
+  secondInput = null;
+  operatorType = null;
+  calculatorInput.textContent = firstInput;
+  resultDisplayed = true;
+});
+clearKey.addEventListener("click", () => {
+  firstInput = null;
+  secondInput = null;
+  operatorType = null;
+  clearEl(calculatorInput);
+  resultDisplayed = false;
+});
+backKey.addEventListener("click", () => {
+  const back = calculatorInput.textContent.slice(0, -1);
+  calculatorInput.textContent = back;
 });
